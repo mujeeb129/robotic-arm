0b3c746a5db8c81d6e5b852b15a4c07ee6f4efc4
Mujeeb Rahman <mujeebmj313@gmail.com>
2021-12-13 22:52:58 +0530

    Face detection completed and studied

--- source_code/main.py
+++ source_code/main.py
@@ -2,19 +2,27 @@ import cv2 as cv
 import numpy as np
 
 #to start capturing video
+faceCascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 cap = cv.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
 
-faceCascade = cv.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
 
 while True:
     ret, img = cap.read()
-    gray = cv.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor = 1.2,
-        minNeighbors = 5,
-        minSize = (20,20)
-    )
-    for(x,y,w,h) in faces:
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, 1.2, 5)
+    for (x,y,w,h) in faces:
+        cv.rectangle(img, (x,y), (x+w,y+h), (255,0,0) , 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+    cv.imshow('video', img)
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv.destroyAllWindows()
+
+
+        
