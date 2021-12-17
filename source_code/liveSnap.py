98ce2fa4adbb11c915dd6a7d6a374fc3a3b6fbc8
Mujeeb Rahman <mujeebmj313@gmail.com>
2021-12-18 00:29:32 +0530

    Live snap function is written to take pictures on live

--- source_code/liveSnap.py
+++ source_code/liveSnap.py
@@ -1,3 +1,23 @@
 import os
 import cv2 as cv
 
+cap = cv.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+count = 0
+while True:
+    ret,img = cap.read()
+    name = 'dataset/'+str(count) + '.jpg'
+    cv.imshow('camera' , img)
+    k = cv.waitKey(33) & 0xff
+    if k == 27:
+        break
+    elif k == 115:
+        cv.imwrite(name, img)
+        count += 1
+
+cap.release()
+cv.destroyAllWindows()
+    
+    
+
