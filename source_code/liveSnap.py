5b1dbc3ee7ecc97383c9490ba73b546827b6b07a
Mujeeb Rahman <mujeebmj313@gmail.com>
2021-12-18 01:03:42 +0530

    Changes has been made

--- source_code/liveSnap.py
+++ source_code/liveSnap.py
@@ -1,23 +1,33 @@
 import os
 import cv2 as cv
 
-cap = cv.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
-count = 0
-while True:
-    ret,img = cap.read()
-    name = 'dataset/'+str(count) + '.jpg'
-    cv.imshow('camera' , img)
-    k = cv.waitKey(33) & 0xff
-    if k == 27:
-        break
-    elif k == 115:
-        cv.imwrite(name, img)
-        count += 1
+def LiveSnap():
+    subName = input('Enter subject number : ')
 
-cap.release()
-cv.destroyAllWindows()
-    
-    
+    if not os.path.isdir('dataset/' + subName):
+        os.makedirs('dataset/' + subName)
+    else:
+        print('Subject {} already exists'.format(subName))
+        return
 
+    pathImg = 'dataset/{}/'.format(subName)
+
+    cap = cv.VideoCapture(2)
+    cap.set(3,640)
+    cap.set(4,480)
+    count = 0
+    while True:
+        ret,img = cap.read()
+        name = 'dataset/{}/{}_{}.jpg'.format(subName,subName,count)
+        cv.imshow('camera' , img)
+        k = cv.waitKey(33) & 0xff
+        if k == 27:
+            break
+        elif k == 115:
+            cv.imwrite(name, img)
+            count += 1
+
+    cap.release()
+    cv.destroyAllWindows()
+
+LiveSnap()
