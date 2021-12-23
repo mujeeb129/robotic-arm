508fba02e6e51c4221238a8a725e360856e114d2
Mujeeb Rahman <mujeebmj313@gmail.com>
2021-12-24 00:21:16 +0530

    minor changes

--- source_code/faceDetection.py
+++ source_code/faceDetection.py
@@ -2,25 +2,27 @@ import cv2 as cv
 import numpy as np
 
 class FaceDetection():
-    def startDetection():
+    def startDetection(self):
         self.faceCascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
-        self.cap = cv.VideoCapture(2)
-        self.cap.set(3,640)
-        self.cap.set(4,480)
+        cap = cv.VideoCapture(0)
+        cap.set(3,640)
+        cap.set(4,480)
         
         while True:
-            ret, img = self.cap.read()
-            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-            faces = self.faceCascade.detectMultiScale(gray, 1.2, 5)
-            for (x,y,w,h) in faces:
-                cv.rectangle(img, (x,y), (x+w,y+h), (255,0,0) , 2)
-                roi_gray = gray[y:y+h, x:x+w]
-                roi_color = img[y:y+h, x:x+w]
-                cv.imshow('video', img)
-                k = cv.waitKey(30) & 0xff
-                if k == 27:
+            self.ret, self.img = cap.read()
+            self.gray = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY)
+            self.faces = self.faceCascade.detectMultiScale(self.gray, 1.2, 5)
+            for (x,y,w,h) in self.faces:
+                cv.rectangle(self.img, (x,y), (x+w,y+h), (255,0,0) , 2)
+                roi_gray = self.gray[y:y+h, x:x+w]
+                roi_color = self.img[y:y+h, x:x+w]
+                cv.imshow('video', self.img)
+                self.k = cv.waitKey(30) & 0xff
+                if self.k == 27:
                     break
 
         cap.release()
         cv.destroyAllWindows()
 
+x = FaceDetection()
+x.startDetection()
